ee0cbffe469b550c0233c23acdfab2ca51e7001b
FlyingAnt <flying2010.liu@gmail.com>
2016-12-20 16:59:53 +0800

    Bug fixed

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = validate({
 
     resolve: {
-        extensions: ['.json', '.js']
+        extensions: ['', '.json', '.js']
     },
 
     output: {
